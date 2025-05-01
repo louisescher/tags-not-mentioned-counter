d26719304abeb1f7653270cfe9890408eb6634a7
Louis Escher <louisescher@proton.me>
2025-05-01 15:19:33 +0200

    fix: wrong header name and wrong output mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import db from '@astrojs/db';
 // https://astro.build/config
 export default defineConfig({
   site: "https://tags.lou.gg",
+  output: "server",
   adapter: node({
     mode: 'standalone'
   }),
--- src/pages/api/reset.ts
+++ src/pages/api/reset.ts
@@ -2,7 +2,7 @@ import type { APIRoute } from "astro";
 import { counter, db } from "astro:db";
 
 export const GET: APIRoute = async (ctx) => {
-	const authHeader = ctx.request.headers.get("Authentication");
+	const authHeader = ctx.request.headers.get("Authorization");
 
 	if (!authHeader || authHeader !== `Bearer ${import.meta.env.AUTH_TOKEN}`) {
 		return new Response("Unauthorized", { status: 401 });
